32f5f6cbeed61c3fd82e152147b4e4f1382ff861
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-10-03 20:00:17 +0530

    Added Global Styles, Fonts, Icons, Preloader, and Hero
    - Added global styles for a consistent design.
    - Integrated custom fonts and icons.
    - Implemented a preloader for better user experience during loading.
    - Created a hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,25 @@
-import * as React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Hero from '../components/sections/Hero';
+import Layout from '../components/layout';
 
-const IndexPage = () => {
-  return <main></main>;
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
+
+const IndexPage = ({ location }) => (
+  <Layout location={location}>
+    <StyledMainContainer className="fillHeight">
+      <Hero />
+     
+    </StyledMainContainer>
+  </Layout>
+);
+
+IndexPage.propTypes = {
+  location: PropTypes.object.isRequired,
 };
 
 export default IndexPage;
-
-export const Head = () => <title>Home Page</title>;
