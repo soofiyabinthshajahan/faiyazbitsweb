887bb6ba1e63f8a624d45750c38d9b7f34f19982
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-10-05 19:28:33 +0530

    Created NavBar

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,7 +135,7 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
+              Hello! My name is Faiayz and I enjoy creating things that live on the internet. My
               interest in web development started back in 2012 when I decided to try editing custom
               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
               about HTML &amp; CSS!
@@ -143,18 +143,18 @@ const About = () => {
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              <a href="#">an advertising agency</a>,{' '}
+              <a href="#">a start-up</a>,{' '}
+              <a href="#">a huge corporation</a>, and{' '}
+              <a href="#">a student-led design studio</a>. My
               main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              experiences at <a href="#">Upstatement</a> for a variety of
               clients.
             </p>
 
             <p>
               I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
+              <a href="#">
                 launched a course
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
